44824079bd27370c7c928d191f4d68ccece570c7
nakagawach <85813915+nakagawach@users.noreply.github.com>
2026-08-14 23:11:34 +0900

    Test pointerup activation for touch buttons

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ImmediateTouchButtons from './ImmediateTouchButtons.tsx'
 import UpdatePrompt from './UpdatePrompt.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <UpdatePrompt />
+    <ImmediateTouchButtons>
+      <App />
+      <UpdatePrompt />
+    </ImmediateTouchButtons>
   </StrictMode>,
 )
