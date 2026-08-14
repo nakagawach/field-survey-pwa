64b02a1a22c5be8c7363dc0cd4781c1c6dd826d9
nakagawach <85813915+nakagawach@users.noreply.github.com>
2026-08-14 17:46:00 +0900

    Replace production photo viewer with PhotoSwipe

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import UpdatePrompt from './UpdatePrompt.tsx'
-import PhotoSwipePoc from './PhotoSwipePoc.tsx'
-
-const isPhotoSwipePoc = window.location.pathname.endsWith('/photoswipe-poc')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {isPhotoSwipePoc ? (
-      <PhotoSwipePoc />
-    ) : (
-      <>
-        <App />
-        <UpdatePrompt />
-      </>
-    )}
+    <App />
+    <UpdatePrompt />
   </StrictMode>,
 )
