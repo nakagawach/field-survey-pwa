175be0a5278e3b862fe4974f4b1ed525a9a59d48
nakagawach <85813915+nakagawach@users.noreply.github.com>
2026-08-14 23:33:04 +0900

    Test photo library picker on pointerdown

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import DirectPhotoLibraryPointerUp from './DirectPhotoLibraryPointerUp.tsx'
 import ImmediateTouchButtons from './ImmediateTouchButtons.tsx'
+import PhotoLibraryPointerDown from './PhotoLibraryPointerDown.tsx'
 import UpdatePrompt from './UpdatePrompt.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ImmediateTouchButtons>
-      <DirectPhotoLibraryPointerUp />
-      <App />
-      <UpdatePrompt />
+      <PhotoLibraryPointerDown>
+        <App />
+        <UpdatePrompt />
+      </PhotoLibraryPointerDown>
     </ImmediateTouchButtons>
   </StrictMode>,
 )
