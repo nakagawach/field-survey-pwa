01ea2b490d634c762c2a2c5a4068263f166633da
nakagawach <85813915+nakagawach@users.noreply.github.com>
2026-08-14 23:28:11 +0900

    Wire direct photo library pointerup test

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import DirectPhotoLibraryPointerUp from './DirectPhotoLibraryPointerUp.tsx'
 import ImmediateTouchButtons from './ImmediateTouchButtons.tsx'
 import UpdatePrompt from './UpdatePrompt.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ImmediateTouchButtons>
+      <DirectPhotoLibraryPointerUp />
       <App />
       <UpdatePrompt />
     </ImmediateTouchButtons>
