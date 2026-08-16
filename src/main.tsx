e4068c4cf201a1bcb3bdd6e44b79e5c3c414e55b
nakagawach <85813915+nakagawach@users.noreply.github.com>
2026-08-16 12:54:34 +0900

    Switch app to clean v1 implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import ImmediateTouchButtons from './ImmediateTouchButtons.tsx'
-import PhotoLibraryPointerDown from './PhotoLibraryPointerDown.tsx'
+import './v1/styles.css'
+import App from './v1/App.tsx'
 import UpdatePrompt from './UpdatePrompt.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ImmediateTouchButtons>
-      <PhotoLibraryPointerDown>
-        <App />
-        <UpdatePrompt />
-      </PhotoLibraryPointerDown>
-    </ImmediateTouchButtons>
+    <App />
+    <UpdatePrompt />
   </StrictMode>,
 )
