5359426ce72c6ad060152bc6b847cfd76d5f6d2d
nakagawach <85813915+nakagawach@users.noreply.github.com>
2026-08-16 11:07:20 +0900

    Restore immediate touch button handling

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ImmediateTouchButtons from './ImmediateTouchButtons.tsx'
 import PhotoLibraryPointerDown from './PhotoLibraryPointerDown.tsx'
 import UpdatePrompt from './UpdatePrompt.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PhotoLibraryPointerDown>
-      <App />
-      <UpdatePrompt />
-    </PhotoLibraryPointerDown>
+    <ImmediateTouchButtons>
+      <PhotoLibraryPointerDown>
+        <App />
+        <UpdatePrompt />
+      </PhotoLibraryPointerDown>
+    </ImmediateTouchButtons>
   </StrictMode>,
 )
