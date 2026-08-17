6abf93db7fa302a4c7fcbebe21bc7cd22fcff5f7
nakagawach <85813915+nakagawach@users.noreply.github.com>
2026-08-17 21:12:52 +0900

    Load scoped v1.0.2 styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './clean4/App'
 import TouchBridge from './clean4/TouchBridge'
 import UpdatePrompt from './UpdatePrompt'
+import './clean4/v102.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
